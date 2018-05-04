2f3da9068922e11f09b42e7d7ff6cce6c04fcd31
chenqp <13542427@qq.com>
2018-05-04 14:44:50 +0800

    first Commit

---
+++ DigitRecognition/ReadMe.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * 
+ * 
+ * 
+   TestResources
+    │ 
+    ├──t-1.bmp Success，白底黑字，56识别为56
+    ├──t-2.bmp Fail，彩色的底黑色的字直接报错；
+    ├──t-3.bmp Success，白底彩色的字识别正确，78识别为78
+    ├──t-4.bmp Fail，32识别为55
+    ├──t-5.bmp Success，57识别为57
+    ├──t-6.bmp Success，685识别为685
+    ├──t-7.bmp Fail，54识别为55
+    ├──t-8.bmp Fail，12识别为25
+    ├──t-9.bmp Fail，78识别为75
+    ├──t-10.png Fail，56识别为55
+    ├──t-11.jpg Success，12识别为12
+    ├──t-12.jpg Success，34识别为34
+    ├──t-13.jpg Success，7识别为7
+    ├──t-14.jpg Success，56识别为56
+    ├──t-15.jpg Success，5识别为5
+    ├──t-16.jpg Success，8识别为8
+    ├──t-17.jpg Success，9识别为9
+    ├──t-18.jpg Success，10识别为10
+    │
+    └─ 12/18 = 66.67%
+ * 
+ * 
+ * 调用示例：DigitRecognition.exe d:\TestResources\t-18.jpg
+ */ 
